331b5e8dcbd697d26faec82f6c655f1384ffe661
Vedaiyappan Suruttaiyan <vedaiyappans@stellarit.com>
2020-08-15 01:57:08 +0530

    Added MVC to APP

--- Veda-bookStore/Veda-bookStore/Startup.cs
+++ Veda-bookStore/Veda-bookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Veda_bookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,22 +44,31 @@ namespace Veda_bookStore
 
             app.UseRouting();
 
-       
             app.UseEndpoints(endpoints =>
             {
-                endpoints.Map("/", async context =>
-                {
-                    if (env.IsEnvironment("Develop"))
-                    {
-                        await context.Response.WriteAsync("Hello World! DEV");
-                    }
-                    else
-                    {
-                        await context.Response.WriteAsync(env.EnvironmentName);
-                    }
-
-                });
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.MapControllerRoute(
+                //    name: "Default",
+                //    pattern: "bookApp/{controller=Home}/{action=Index}/{id?}");
             });
+
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/", async context =>
+            //    {
+            //        if (env.IsEnvironment("Develop"))
+            //        {
+            //            await context.Response.WriteAsync("Hello World! DEV");
+            //        }
+            //        else
+            //        {
+            //            await context.Response.WriteAsync(env.EnvironmentName);
+            //        }
+
+            //    });
+            //});
+
             //app.UseEndpoints(endpoints =>
             //{
             //    endpoints.Map("/veda", async context =>
